b91b22de420299229e2c46c3cd2f988debccd04b
nhd98z <nhd98z@gmail.com>
2024-11-17 11:35:03 +0700

    152

---
+++ src/bin/152.rs
@@ -0,0 +1,28 @@
+use std::cmp::max;
+
+#[allow(dead_code)]
+
+impl Solution {
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        let mut res = nums[0];
+        let mut cmin = nums[0];
+        let mut cmax = nums[0];
+        for i in 1..nums.len() {
+            let candidates = [nums[i], cmin * nums[i], cmax * nums[i]];
+            cmin = candidates.into_iter().min().unwrap();
+            cmax = candidates.into_iter().max().unwrap();
+            res = max(res, cmax);
+        }
+        res
+    }
+}
+
+struct Solution;
+
+fn main() {
+    dbg!(Solution::max_product(vec![2, 3, -2, 4]));
+    dbg!(Solution::max_product(vec![-2, 0, -1]));
+    dbg!(Solution::max_product(vec![-3, -1, -1]));
+    dbg!(Solution::max_product(vec![0, 2]));
+
+}
