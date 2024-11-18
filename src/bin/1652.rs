e27a94a13b41d9e5070331d7b753f2603f27b1e6
nhd98z <nhd98z@gmail.com>
2024-11-18 07:32:24 +0700

    1652

---
+++ src/bin/1652.rs
@@ -0,0 +1,39 @@
+#[allow(dead_code)]
+
+impl Solution {
+    pub fn decrypt(code: Vec<i32>, k: i32) -> Vec<i32> {
+        let len = code.len();
+        let mut res = vec![0; len];
+        match k {
+            0 => {}
+            _ if k < 0 => {
+                for i in 0..len {
+                    let i = i as i32;
+                    let mut sum = 0;
+                    for j in i + k..=i - 1 {
+                        sum += code[((j + len as i32) as usize) % len];
+                    }
+                    res[i as usize] = sum;
+                }
+            }
+            _ => {
+                for i in 0..len {
+                    let mut sum = 0;
+                    for j in i + 1..=i + k as usize {
+                        sum += code[j % len];
+                    }
+                    res[i] = sum;
+                }
+            }
+        };
+        res
+    }
+}
+
+struct Solution;
+
+fn main() {
+    dbg!(Solution::decrypt(vec![5, 7, 1, 4], 3));
+    dbg!(Solution::decrypt(vec![1, 2, 3, 4], 0));
+    dbg!(Solution::decrypt(vec![2, 4, 9, 3], -2));
+}
