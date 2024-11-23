031b50c678798edbd91cfb54fb91b598ef3ebd40
nhd98z <nhd98z@gmail.com>
2024-11-23 19:19:13 +0700

    Definitely faster 698

--- src/bin/698.rs
+++ src/bin/698.rs
@@ -3,39 +3,90 @@
 use rand::Rng;
 use std::time::{SystemTime, UNIX_EPOCH};
 
+// impl Solution {
+//     pub fn can_partition_k_subsets(nums: Vec<i32>, k: i32) -> bool {
+//         let sum: i32 = nums.iter().sum();
+//         if sum % k != 0 {
+//             return false;
+//         }
+//         let target = sum / k;
+//         if nums.iter().any(|&num| num > target) {
+//             return false;
+//         }
+//         let mut nums = nums;
+//         nums.sort_unstable_by(|a, b| b.cmp(a)); // Sort in decreasing order
+//         let mut subsets = vec![0; k as usize];
+//         Self::dfs(&nums, &mut subsets, 0, target)
+//     }
+
+//     fn dfs(nums: &Vec<i32>, subsets: &mut Vec<i32>, pos: i32, target: i32) -> bool {
+//         if pos == nums.len() as i32 {
+//             return subsets.iter().all(|&sum| sum == target);
+//         }
+//         for i in 0..subsets.len() {
+//             if subsets[i] + nums[pos as usize] > target {
+//                 continue;
+//             }
+//             if subsets[..i].iter().any(|&sum| sum == subsets[i]) {
+//                 continue;
+//             }
+//             subsets[i] += nums[pos as usize];
+//             if Self::dfs(&nums, subsets, pos + 1, target) {
+//                 return true;
+//             }
+//             subsets[i] -= nums[pos as usize];
+//         }
+//         false
+//     }
+// }
+
 impl Solution {
     pub fn can_partition_k_subsets(nums: Vec<i32>, k: i32) -> bool {
+        let n = nums.len();
         let sum: i32 = nums.iter().sum();
         if sum % k != 0 {
             return false;
         }
         let target = sum / k;
-        if nums.iter().any(|&num| num > target) {
+        if nums.iter().any(|&num| num > target) == true {
             return false;
         }
         let mut nums = nums;
         nums.sort_unstable_by(|a, b| b.cmp(a)); // Sort in decreasing order
-        let mut subsets = vec![0; k as usize];
-        Self::dfs(&nums, &mut subsets, 0, target)
+        let mut memo = vec![None; 1 << n];
+        Self::dfs(&nums, target, k, 0, 0, &mut memo)
     }
 
-    fn dfs(nums: &Vec<i32>, subsets: &mut Vec<i32>, pos: i32, target: i32) -> bool {
-        if pos == nums.len() as i32 {
-            return subsets.iter().all(|&sum| sum == target);
+    fn dfs(
+        nums: &[i32],
+        target: i32,
+        k: i32,
+        curr_sum: i32,
+        mask: usize,
+        memo: &mut [Option<bool>],
+    ) -> bool {
+        let n = nums.len();
+        if k == 1 {
+            memo[mask] = Some(true);
+            return true;
         }
-        for i in 0..subsets.len() {
-            if subsets[i] + nums[pos as usize] > target {
-                continue;
-            }
-            if subsets[..i].iter().any(|&sum| sum == subsets[i]) {
-                continue;
-            }
-            subsets[i] += nums[pos as usize];
-            if Self::dfs(&nums, subsets, pos + 1, target) {
-                return true;
-            }
-            subsets[i] -= nums[pos as usize];
+        if let Some(cache) = memo[mask] {
+            return cache;
         }
+        if curr_sum == target {
+            let res = Self::dfs(nums, target, k - 1, 0, mask, memo);
+            memo[mask] = Some(res);
+            return res;
+        }
+        for i in 0..n {
+            if mask & (1 << i) == 0 && curr_sum + nums[i] <= target {
+                if Self::dfs(nums, target, k, curr_sum + nums[i], mask | (1 << i), memo) {
+                    memo[mask] = Some(true);
+                    return true;
+                }
+            }
+        }
+        memo[mask] = Some(false);
         false
     }
 }
