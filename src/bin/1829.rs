e4b9fe74cdab054f98509151a4d5bee75c5ff4b7
nhd98z <nhd98z@gmail.com>
2024-11-24 02:14:48 +0700

    1829

---
+++ src/bin/1829.rs
@@ -0,0 +1,22 @@
+#![allow(dead_code, unused)]
+
+impl Solution {
+    pub fn get_maximum_xor(nums: Vec<i32>, maximum_bit: i32) -> Vec<i32> {
+        let n = nums.len();
+        let mask = (1 << maximum_bit) - 1;
+        let mut res = vec![0; n];
+        let mut curr = 0;
+        for i in 0..n {
+            curr ^= nums[i];
+            res[n - i - 1] = curr ^ mask;
+        }
+        res
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(Solution::get_maximum_xor(vec![0, 1, 1, 3], 2), [0, 3, 2, 3]);
+    assert_eq!(Solution::get_maximum_xor(vec![2, 3, 4, 7], 3), [5, 2, 6, 5]);
+}
