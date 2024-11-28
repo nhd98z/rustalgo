c25b78e2ee1533108c3bd70c30af2717f9b7e89f
nhd98z <nhd98z@gmail.com>
2024-11-29 01:30:27 +0700

    64 and 5

---
+++ src/bin/5.rs
@@ -0,0 +1,63 @@
+#![allow(dead_code, unused)]
+
+impl Solution {
+    pub fn longest_palindrome(s: String) -> String {
+        // O(n^2)
+        let mut s: Vec<u8> = s.bytes().into_iter().collect();
+        let n = s.len() as i32;
+        let mut res: String = (s[0] as char).to_string();
+        for i in 0..n {
+            let mut j = i;
+            let mut k = i;
+            while j >= 0 && k < n && s[j as usize] == s[k as usize] {
+                if k - j + 1 > res.len() as i32 {
+                    res = String::from_utf8(s[j as usize..=k as usize].to_vec()).unwrap();
+                }
+                j -= 1;
+                k += 1;
+            }
+            let mut j = i;
+            let mut k = i + 1;
+            while j >= 0 && k < n && s[j as usize] == s[k as usize] {
+                if k - j + 1 > res.len() as i32 {
+                    res = String::from_utf8(s[j as usize..=k as usize].to_vec()).unwrap();
+                }
+                j -= 1;
+                k += 1;
+            }
+        }
+        res
+    }
+    pub fn longest_palindrome_manacher(s: String) -> String {
+        todo!()
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(
+        Solution::longest_palindrome("babad".to_string()),
+        "bab".to_string()
+    );
+    assert_eq!(
+        Solution::longest_palindrome("cbbd".to_string()),
+        "bb".to_string()
+    );
+    assert_eq!(
+        Solution::longest_palindrome("a".to_string()),
+        "a".to_string()
+    );
+    assert_eq!(
+        Solution::longest_palindrome("aa".to_string()),
+        "aa".to_string()
+    );
+    assert_eq!(
+        Solution::longest_palindrome("aab".to_string()),
+        "aa".to_string()
+    );
+    assert_eq!(
+        Solution::longest_palindrome("aba".to_string()),
+        "aba".to_string()
+    );
+}
---
+++ src/bin/64.rs
@@ -0,0 +1,41 @@
+#![allow(dead_code, unused)]
+
+use std::i32;
+
+impl Solution {
+    pub fn min_path_sum(grid: Vec<Vec<i32>>) -> i32 {
+        let m = grid.len();
+        let n = grid[0].len();
+        let mut f = vec![vec![0; n]; m];
+        for i in 0..m {
+            for j in 0..n {
+                let mut min = i32::MAX;
+                if i >= 1 {
+                    min = f[i - 1][j];
+                }
+                if j >= 1 {
+                    min = i32::min(min, f[i][j - 1]);
+                }
+                if min == i32::MAX {
+                    f[i][j] = grid[i][j];
+                } else {
+                    f[i][j] = min + grid[i][j];
+                }
+            }
+        }
+        f[m - 1][n - 1]
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(
+        Solution::min_path_sum(vec![vec![1, 3, 1], vec![1, 5, 1], vec![4, 2, 1]]),
+        7
+    );
+    assert_eq!(
+        Solution::min_path_sum(vec![vec![1, 2, 3], vec![4, 5, 6]]),
+        12
+    );
+}
