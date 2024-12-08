14a95c7ec0ec58d16b17f3b6ae0619a40eb73309
nhd98z <nhd98z@gmail.com>
2024-12-08 19:19:34 +0700

    Add alternative implementation for longest common subsequence

--- src/bin/leetcode_1143.rs
+++ src/bin/leetcode_1143.rs
@@ -17,6 +17,23 @@ impl Solution {
         }
         dp[b.len()]
     }
+    // Another approach that can apply to LCIS problem.
+    pub fn longest_common_subsequence_2(text1: String, text2: String) -> i32 {
+        let a = text1.as_bytes();
+        let b = text2.as_bytes();
+        let mut dp = vec![0; b.len()];
+        for i in 0..a.len() {
+            let mut curr = 0;
+            for j in 0..b.len() {
+                let prev = curr;
+                curr = usize::max(curr, dp[j]);
+                if a[i] == b[j] {
+                    dp[j] = usize::max(dp[j], prev + 1);
+                }
+            }
+        }
+        dp.into_iter().max().unwrap() as i32
+    }
 }
 
 struct Solution;
@@ -30,4 +47,14 @@ fn main() {
         5
     );
     assert_eq!(Solution::longest_common_subsequence("bsbininm".to_string(), "jmjkbkjkv".to_string()), 1);
+
+
+    assert_eq!(Solution::longest_common_subsequence_2("abcde".to_string(), "ace".to_string()), 3);
+    assert_eq!(Solution::longest_common_subsequence_2("abc".to_string(), "abc".to_string()), 3);
+    assert_eq!(Solution::longest_common_subsequence_2("abc".to_string(), "def".to_string()), 0);
+    assert_eq!(
+        Solution::longest_common_subsequence_2("abcba".to_string(), "abcbcba".to_string()),
+        5
+    );
+    assert_eq!(Solution::longest_common_subsequence_2("bsbininm".to_string(), "jmjkbkjkv".to_string()), 1);
 }
