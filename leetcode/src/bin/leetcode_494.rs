0a1e458b5b707cc665bab53a03a9e47073b81851
nhd98z <nhd98z@gmail.com>
2024-12-26 09:53:53 +0700

    Smart leetcode_494

--- leetcode/src/bin/leetcode_494.rs
+++ leetcode/src/bin/leetcode_494.rs
@@ -1,18 +1,40 @@
 #![allow(dead_code)]
 
 impl Solution {
-    fn find(nums: &Vec<i32>, target: i32, sum: i32, pos: usize) -> i32 {
-        let n = nums.len();
-        if pos == n {
-            if sum == target {
-                return 1;
+    pub fn find_target_sum_ways_stupid(nums: Vec<i32>, target: i32) -> i32 {
+        fn find(nums: &Vec<i32>, target: i32, sum: i32, pos: usize) -> i32 {
+            let n = nums.len();
+            if pos == n {
+                if sum == target {
+                    return 1;
+                }
+                return 0;
             }
-            return 0;
+            find(&nums, target, sum - &nums[pos], pos + 1)
+                + find(&nums, target, sum + &nums[pos], pos + 1)
         }
-        Self::find(&nums, target, sum - &nums[pos], pos + 1) + Self::find(&nums, target, sum + &nums[pos], pos + 1)
-    }    
+        find(&nums, target, 0, 0)
+    }
+
     pub fn find_target_sum_ways(nums: Vec<i32>, target: i32) -> i32 {
-        Self::find(&nums, target, 0, 0)
+        let t_sum = nums.iter().sum::<i32>();
+        if t_sum < target || -t_sum > target || (t_sum & 1) ^ (target & 1) == 1 {
+            0
+        } else {
+            let limit = ((t_sum + target) >> 1) as usize;
+            let mut dp = vec![0; limit + 1];
+            dp[0] = 1;
+            for i in 1..=nums.len() {
+                for j in (0..=limit).rev() {
+                    dp[j] += if j >= nums[i - 1] as usize {
+                        dp[j - nums[i - 1] as usize]
+                    } else {
+                        0
+                    }
+                }
+            }
+            dp[limit]
+        }
     }
 }
 
