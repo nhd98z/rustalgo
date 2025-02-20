0ca07d1f628d5b8ab67a94e426176b88fee8c01a
nhd98z <nhd98z@gmail.com>
2025-02-21 00:06:57 +0700

    1980: Implement binary string differentiation using Cantor's diagonalization

---
+++ leetcode/src/bin/leetcode_1980.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    pub fn find_different_binary_string(nums: Vec<String>) -> String {
+        // Use Cantor's diagonalization: for each string's i-th position,
+        // pick the opposite bit for our result's i-th position
+        nums.iter()
+            .enumerate()
+            .map(|(i, num)| {
+                // Get the i-th character and flip it
+                if num.chars().nth(i).unwrap() == '0' {
+                    '1'
+                } else {
+                    '0'
+                }
+            })
+            .collect()
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(
+        Solution::find_different_binary_string(vec!["01".to_string(), "10".to_string()]),
+        "11"
+    );
+}
