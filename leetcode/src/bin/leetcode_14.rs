aca07ece1b12ceed1acdd6cae3370eb1432af780
nhd98z <nhd98z@gmail.com>
2025-02-24 14:46:50 +0700

    14

---
+++ leetcode/src/bin/leetcode_14.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        let mut prefix = strs[0].clone();
+        for s in strs.iter().skip(1) {
+            while !s.starts_with(&prefix) {
+                prefix = prefix[..prefix.len() - 1].to_string();
+            }
+        }
+        prefix
+    }
+}
+
+macro_rules! s {
+    ($str:expr) => {
+        String::from($str)
+    };
+}
+
+struct Solution;
+
+fn main() {
+    std::env::set_var("RUST_BACKTRACE", "1");
+    assert_eq!(Solution::longest_common_prefix(vec![s!("flower"), s!("flow"), s!("flight")]), s!("fl"));
+    assert_eq!(Solution::longest_common_prefix(vec![s!("dog"), s!("racecar"), s!("car")]), s!(""));
+}
