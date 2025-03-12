c33f1d75b313b0f5c7da7366328a8b4f9dc87077
nhd98z <nhd98z@gmail.com>
2025-03-12 11:09:20 +0700

    2529

---
+++ leetcode/src/bin/leetcode_2529.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn maximum_count(nums: Vec<i32>) -> i32 {
+        let mut l = 0;
+        let mut r = 0;
+        for num in nums {
+            if num < 0 {
+                l += 1;
+            } else if num > 0 {
+                r += 1;
+            }
+        }
+        i32::max(l, r)
+    }
+}
+
+struct Solution;
+
+fn main() {
+    unsafe { std::env::set_var("RUST_BACKTRACE", "1"); }
+    assert_eq!(Solution::maximum_count(vec![-2,-1,-1,1,2,3]), 3);
+}
